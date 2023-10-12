f6861d072e26ea650329408f3cbb8d0a585f7b70
Arman <armanbarseghyan83@gmail.com>
2023-10-12 10:33:13 -0700

    Array of questions crated

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,61 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'Your project title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Your project description? ',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Your project installation instructions? ',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Your project usage information? ',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Your project contribution guidelines?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Your project test instructions?',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'Your project test instructions?',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'Choose a license',
+        name: 'license',
+        choices: ['No License', 'GNU AGPLv3', 'Boost Software 1.0', 'Mozilla Public 2.0', 'Apache 2.0', 'MIT']
+    },
+    {
+        type: 'input',
+        message: 'Your GitHub username?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Your email address?',
+        name: 'email'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
