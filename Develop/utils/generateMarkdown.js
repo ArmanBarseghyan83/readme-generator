1c2f95e14c9d4c18eef9eff1681fdfa43309559e
Arman <armanbarseghyan83@gmail.com>
2023-10-12 11:15:48 -0700

    generateMarkdown() function created

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,8 +16,7 @@ function renderLicenseLink(license) {
   return `[${license}](https://choosealicense.com/licenses/${params}/)`;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function that returns the license section of README
 function renderLicenseSection(license) {
   const title = '## License\n';
   const notice = `### This application is covered under ${license} license\n`;
@@ -26,11 +25,36 @@ function renderLicenseSection(license) {
   return title + notice + link;
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `
+  const title = data.title ? `# ${data.title}\n` : '';
+  const description = data.description ? `## Description\n${data.description}\n` : '';
+  const installation = data.installation ? `## Installation\n${data.installation}\n` : '';
+  const usage = data.usage ? `## Usage\n${data.usage}\n` : '';
+  const contribution = data.contribution ? `## Contributing\n${data.contribution}\n` : '';
+  const test = data.test ? `## Tests\n${data.test}\n` : '';
+  const badge = data.license !== 'No License' ? renderLicenseBadge(data.license) : '';
+  const license = data.license !== 'No License' ? renderLicenseSection(data.license) : '';
 
-`;
+  const gitHubLink = data.username ? `- GitHub Profile - [https://github.com/${data.username}](https://github.com/${data.username})` : '';
+  const email = data.email ? `- Email Address - [${data.email}](mailto:${data.email})` : '';
+  const questionsTitle = 'Use the links below if you have any questions.';
+  const questions = data.email || data.username ? `## Questions\n### ${questionsTitle}\n${gitHubLink}\n${email}` : '';
+
+  const contentsList = [];
+  data.description && contentsList.push('- [Description](#description)\n');
+  data.installation && contentsList.push('- [Instalation](#installation)\n');
+  data.usage && contentsList.push('- [Usage](#usage)\n');
+  data.license !== 'No License' && contentsList.push('- [License](#license)\n');
+  data.contribution && contentsList.push('- [Contributing](#contributing)\n');
+  data.test && contentsList.push('- [Tests](#tests)\n');
+  questions && contentsList.push('- [Questions](#questions)\n');
+
+  const contents = `## Table of Contents\n${contentsList.join('')}\n`;
+
+  const markdown = title + badge + description + contents + installation + usage + license + contribution + test + questions;
+
+  return markdown;
 }
 
 module.exports = generateMarkdown;
